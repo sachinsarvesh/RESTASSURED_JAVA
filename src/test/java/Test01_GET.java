6c8c774f351b8d959f5d6ff54baed281737d883f
Sarvy <sarvesh240698@gmail.com>
2026-03-28 13:05:45 +0530

    Rest Assured testing framework

---
+++ src/test/java/Test01_GET.java
@@ -0,0 +1,31 @@
+import static io.restassured.RestAssured.*;
+import static org.hamcrest.Matchers.equalTo;
+
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import org.testng.annotations.Test;
+
+public class Test01_GET {
+
+    @Test
+    void test_01(){
+
+        Response response = RestAssured
+                .given()
+                .header("x-api-key", "reqres_b25c3b72c98c4dc3b489978908d2775b")
+                .get("https://reqres.in/api/users?page=2");
+        System.out.println(response.getStatusCode());
+        System.out.println(response.getBody().asString());
+
+    }
+
+    @Test
+    void test_02(){
+        given()
+                .header("x-api-key", "reqres_b25c3b72c98c4dc3b489978908d2775b")
+                .get("https://reqres.in/api/users?page=2")
+                .then()
+                .statusCode(200)
+                .body("data.id[0]",equalTo(8));
+    }
+}
